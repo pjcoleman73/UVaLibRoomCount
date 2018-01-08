ac28adc7dd562ce5a551c1f2493e162bc1971323
Ammon Shepherd <ammon.shepherd@gmail.com>
2018-01-08 15:09:47 -0500

    Adds code to define a person.

--- video/bg.py
+++ video/bg.py
@@ -22,6 +22,8 @@ fgbg = cv2.createBackgroundSubtractorMOG2(detectShadows = True) #Create the back
 
 kernelOp = np.ones((3,3),np.uint8)
 kernelCl = np.ones((11,11),np.uint8)
+areaTH = 500
+
 
 while True:
     frame = cap.read() #read a frame
@@ -48,6 +50,18 @@ while True:
     _, contours0, hierarchy = cv2.findContours(mask,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_NONE)
     for cnt in contours0:
         cv2.drawContours(frame, cnt, -1, (0,255,0), 3, 8)
+        area = cv2.contourArea(cnt)
+        print area
+        if area > areaTH:
+            #################
+            #   TRACKING    #
+            #################            
+            M = cv2.moments(cnt)
+            cx = int(M['m10']/M['m00'])
+            cy = int(M['m01']/M['m00'])
+            x,y,w,h = cv2.boundingRect(cnt)
+            cv2.circle(frame,(cx,cy), 5, (0,0,255), -1)            
+            img = cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,0),2)
 
     cv2.imshow('Frame',frame)
     #Abort and exit with 'Q' or ESC
